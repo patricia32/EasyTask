bd9300ac099a1ed198039cd3e2b4bbe61db85ecc
Patricia Teocan <teocan.patricia03@gmail.com>
2026-04-27 15:45:08 +0300

    user: Add input and output variables with/without signals

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,13 @@
-import { Component, computed, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  signal,
+  Input,
+  input,
+  Output,
+  EventEmitter,
+  output,
+} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 @Component({
@@ -9,23 +18,45 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[0]);
+  // selectedUser = signal(DUMMY_USERS[0]);
 
-  // No signals
+  // -- No signals
   // When a value is changed, Angular checks all the components
 
   // get imagePath() {
   //   return 'assets/users/' + this.selectedUser.avatar;
   // }
 
-  // Signals
+  // -- Signals
   // When a value is changed, Angular only checks the components that use the value
 
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  // onSelectUser() {
+  // This method is based on Zone.js & Angular's changes detection
+  //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  //   this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  // }
+
+  // -- No signals
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  // @Output() select = new EventEmitter();
+  select = output<string>(); // replacement for the line above, but does not create a signal
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
+  // -- Signals - READ ONLY
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
 
   onSelectUser() {
-    // This method is based on Zone.js & Angular's changes detection
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this.select.emit(this.id);
   }
 }
